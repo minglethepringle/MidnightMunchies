b4f8de0794a9774ee643b0b00e3f916443c0edcc
Mingle Li <limingle5@gmail.com>
2024-07-20 19:32:36 -0400

    Environment set up

---
+++ Assets/Scripts/Player/PlayerLookController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLookController : MonoBehaviour
+{
+	public float mouseSensitivity = 200f;
+    
+	private Transform playerBody;
+	private float pitch;
+    
+	// Start is called before the first frame update
+	void Start()
+	{
+		playerBody = transform.parent.transform;
+
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float moveX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float moveY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+		// Yaw: Rotate around Y axis for looking left/right
+		playerBody.Rotate(Vector3.up * moveX);
+        
+		// Pitch: Rotate around X axis for looking up/down
+		pitch -= moveY;
+		pitch = Mathf.Clamp(pitch, -90f, 90f);
+		transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float jumpHeight = 1f;
+    public float gravity = 9.81f;
+    public float airControl = 5f;
+    public float sprintSpeedMultiplier = 1.5f;
+    
+    private CharacterController controller;
+    private Vector3 input;
+    private Vector3 moveDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
+
+        input = ((transform.right * moveHorizontal) + (transform.forward * moveVertical)).normalized;
+
+        input *= moveSpeed;
+
+        if (sprint) input *= sprintSpeedMultiplier;
+
+        if (controller.isGrounded)
+        {
+            moveDirection = input;
+            
+            // We can jump
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+            else
+            {
+                moveDirection.y = 0.0f; 
+            }
+        }
+        else
+        {
+            // We are midair
+            input.y = moveDirection.y;
+
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+
+        // Let gravity affect it. Time is not squared because Move() multiplies with deltaTime
+        moveDirection.y -= (gravity * Time.deltaTime);
+        
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
